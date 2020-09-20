e472067f9dfaa5b46d62c57e2a04aad46736107f
Olivia Mattsson <ol3270ma-s@student.lu.se>
2020-09-20 19:11:25 +0200

    Added code for setting up UDP connection

---
+++ ObjSec/client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+# Constants for UDP transmission
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = b"Hello world"
+
+ # Sets up the socket and binds it to the IP and Port address
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPServerSocket.bind((UDP_IP, UDP_PORT))
+
+# Function for the handshake phase.
+def handshake():
+    return
+
+# Function for the transmission phase.
+def transmission():
+    return
