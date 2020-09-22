55f10ba3892d42a0eac58a1a58e37ba50c84e198
Adrian Göransson <ad8223go-s@student.lu.se>
2020-09-22 09:39:30 +0200

    objsec: server/client communication

--- ObjSec/client.py
+++ ObjSec/client.py
@@ -4,16 +4,27 @@ import sys
 # Constants for UDP transmission
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = b"Hello world"
 
- # Sets up the socket and binds it to the IP and Port address
-UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPServerSocket.bind((UDP_IP, UDP_PORT))
+
+def main():
+    # Sets up the socket and binds it to the IP and Port address
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.connect((UDP_IP, UDP_PORT))
+
+        while True:
+            data = input("Send: ")
+            sock.sendall(data.encode("utf-8"))
+
 
 # Function for the handshake phase. Should use ECDHE!
 def handshake():
     return
 
+
 # Function for the transmission phase.
 def transmission():
     return
+
+
+if __name__ == "__main__":
+    main()
--- ObjSec/server.py
+++ ObjSec/server.py
@@ -4,17 +4,27 @@ import sys
 # Constants for UDP transmission
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = b"Hello world"
 
- # Sets up the socket and binds it to the IP and Port address
-UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPServerSocket.bind((UDP_IP, UDP_PORT))
-print("UDP server up and listening")
+
+def main():
+    # Sets up the socket and binds it to the IP and Port address
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind((UDP_IP, UDP_PORT))
+
+        while True:
+            data, (addr, port) = sock.recvfrom(4096)
+            print(f"{addr}:{port}: {data.decode()}")
+
 
 # Function for the handshake phase. Should use ECDHE!
 def handshake():
     return
 
+
 # Function for the transmission phase.
 def transmission():
     return
+
+
+if __name__ == "__main__":
+    main()
