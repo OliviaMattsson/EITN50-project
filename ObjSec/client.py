1d7095878a7a18a569485c2b8621549057c99470
Adrian Göransson <ad8223go-s@student.lu.se>
2020-09-22 19:50:28 +0200

    Encrypted messages (I hope this is correct but I have no idea...)

--- ObjSec/client.py
+++ ObjSec/client.py
@@ -1,3 +1,11 @@
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+import os
 import socket
 import sys
 
@@ -11,18 +19,67 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.connect((UDP_IP, UDP_PORT))
 
+        private_key, derived_key = handshake(sock)
+
         while True:
             data = input("Send: ")
-            sock.sendall(data.encode("utf-8"))
+
+            # initialization_vector
+            # They do not need to be kept secret and they can be included in a transmitted message.
+            # Each time something is encrypted a new initialization_vector should be generated.
+            iv = os.urandom(16)
+            cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
+
+            p = padding.PKCS7(128).padder()
+            padded_data = p.update(data.encode('utf-8')) + p.finalize()
+
+            e = cipher.encryptor()
+            encrypted = e.update(padded_data) + e.finalize()
+
+            # 0x02 is the "operation" header
+            message = b'\x02' + iv + encrypted
+
+            assert len(message) < 64, "Message size exceeds 64 bytes"
+            sock.sendall(message)
 
 
 # Function for the handshake phase. Should use ECDHE!
-def handshake():
-    return
+def handshake(socket):
+    private_key = ec.generate_private_key(ec.SECP384R1())
+
+    public_key = private_key.public_key()
+
+    public_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.X962,
+        format=serialization.PublicFormat.CompressedPoint,
+    )
+    data = b'\x00' + public_bytes
+    socket.sendall(data)
+
+    data, (addr, port) = socket.recvfrom(64)
+    if data[0] != 0x01:
+        raise "Invalid auth response"
+
+    peer_public_bytes = data[1:]
+    peer_public_key = ec.EllipticCurvePublicKey.from_encoded_point(
+        ec.SECP384R1(),
+        peer_public_bytes
+    )
+
+    shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
+
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'handshake data',
+    ).derive(shared_key)
+
+    return (private_key, derived_key)
 
 
 # Function for the transmission phase.
-def transmission():
+def transmission(key):
     return
 
 
--- ObjSec/server.py
+++ ObjSec/server.py
@@ -1,3 +1,11 @@
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+import os
 import socket
 import sys
 
@@ -11,15 +19,58 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.bind((UDP_IP, UDP_PORT))
 
+        session_keys = {}
+
         while True:
-            data, (addr, port) = sock.recvfrom(4096)
-            print(f"{addr}:{port}: {data.decode()}")
+            data, (addr, port) = sock.recvfrom(64)
+            def log(m): print(f"{addr}:{port} {m}")
+
+            if data[0] == 0x00:
+                log("[auth]")
+                private, derived = handshake(data[1:])
+                session_keys[(addr, port)] = (private, derived)
+
+                pub_bytes = private.public_key().public_bytes(
+                    encoding=serialization.Encoding.X962,
+                    format=serialization.PublicFormat.CompressedPoint,
+                )
+
+                sock.sendto(b'\x01' + pub_bytes, (addr, port))
+            else:
+                private, derived = session_keys[(addr, port)]
+
+                iv = data[1:17]
+
+                cipher = Cipher(algorithms.AES(derived), modes.CBC(iv))
+                d = cipher.decryptor()
+
+                message = data[17:]
+                decrypted = d.update(message) + d.finalize()
+
+                u = padding.PKCS7(128).unpadder()
+
+                unpadded = u.update(decrypted) + u.finalize()
+                log(unpadded.decode('utf-8'))
 
 
 # Function for the handshake phase. Should use ECDHE!
-def handshake():
-    # ECDHE https://cryptography.io/en/latest/hazmat/primitives/asymmetric/ec/?highlight=elliptic#elliptic-curves
-    return
+def handshake(peer_public_bytes):
+    private_key = ec.generate_private_key(ec.SECP384R1())
+    peer_public_key = ec.EllipticCurvePublicKey.from_encoded_point(
+        ec.SECP384R1(),
+        peer_public_bytes
+    )
+
+    shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
+
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'handshake data',
+    ).derive(shared_key)
+
+    return (private_key, derived_key)
 
 
 # Function for the transmission phase.
